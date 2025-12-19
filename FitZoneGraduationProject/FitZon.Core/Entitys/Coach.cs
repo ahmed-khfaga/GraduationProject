2a3fe7728704bcdaf2320db76ecda8563aa4e02c
ahmed khfaga <idkmaster89@gmail.com>
2025-12-19 10:40:32 +0200

    adding user,trainee,coach entity

--- FitZoneGraduationProject/FitZon.Core/Entitys/Coach.cs
+++ FitZoneGraduationProject/FitZon.Core/Entitys/Coach.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FitZone.Core.Entitys
 {
-    internal class Coach
+    public class Coach
     {
+        public int ID { get; set; }
+
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        public int YearsOfExperience { get; set; }
+        public decimal? Rating { get; set; } // 0.0 to 5.0
+        public string About { get; set; }
+        
+       
+        //public string PhotoUrl { get; set; }
+
+        public decimal? Price { get; set; } // Hourly rate
+
+        public DateTime HireDate { get; set; } = DateTime.Now;
+
+
+        public virtual User User { get; set; }
+
+
     }
 }
--- FitZoneGraduationProject/FitZon.Core/Entitys/User.cs
+++ FitZoneGraduationProject/FitZon.Core/Entitys/User.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace FitZone.Core.Entitys
 {
-    internal class User
+    public class User
     {
+        public int ID { get; set; }
+        public string F_Name { get; set; }
+        public string L_Name { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "Trainee", "Coach", "Admin"
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual Trainee Trainee { get; set; }
+        public virtual Coach Coach { get; set; }
+
     }
 }
