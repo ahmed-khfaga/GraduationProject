f13bc1d0f5941f55e61afd154b945e55316465da
ahmed khfaga <idkmaster89@gmail.com>
2025-12-26 13:59:31 +0200

    adding membership with plan and price (standard,premium)

---
+++ FitZoneGraduationProject/FitZon.Core/Entitys/TraineeMembership.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitZone.Core.Entitys
+{
+    public class TraineeMembership
+    {
+        public int ID { get; set; }
+
+
+        [ForeignKey("Trainee")]
+        public int TraineeID { get; set; }
+
+        [ForeignKey("Membership")]
+        public int MembershipID { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime? EndDate { get; set; }
+
+        public virtual Trainee Trainee { get; set; }
+        public virtual Membership Membership { get; set; }
+    }
+}
