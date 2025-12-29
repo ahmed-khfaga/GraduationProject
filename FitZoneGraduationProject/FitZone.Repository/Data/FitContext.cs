3ca48e69da5cac82a7b11678099387195cba29ae
ahmed khfaga <idkmaster89@gmail.com>
2025-12-29 21:31:53 +0200

    initial create dependency injection ctor

---
+++ FitZoneGraduationProject/FitZone.Repository/Data/FitContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitZone.Repository.Data
+{
+    public class FitContext : DbContext
+    {
+        public FitContext(DbContextOptions<FitContext> option):base(option)
+        {
+            
+        }
+
+
+
+
+    }
+}
