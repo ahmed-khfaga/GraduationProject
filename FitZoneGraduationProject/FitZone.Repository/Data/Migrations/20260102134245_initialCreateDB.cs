db185ce9ba126c9f1d6f44bcb9e2183d45a80a24
ahmed khfaga <idkmaster89@gmail.com>
2026-01-02 15:52:13 +0200

    adding migrations

---
+++ FitZoneGraduationProject/FitZone.Repository/Data/Migrations/20260102134245_initialCreateDB.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitZone.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsPremium = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    F_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    L_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MembershipPlans",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MembershipID = table.Column<int>(type: "int", nullable: false),
+                    DurationInDays = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipPlans", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MembershipPlans_Memberships_MembershipID",
+                        column: x => x.MembershipID,
+                        principalTable: "Memberships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Coachs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    YearsOfExperience = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<decimal>(type: "decimal(2,1)", precision: 2, scale: 1, nullable: true),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(3,2)", precision: 3, scale: 2, nullable: true),
+                    HireDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coachs", x => x.ID);
+                    table.CheckConstraint("CK_Coach_Rating", "[Rating] >= 0 AND [Rating] <= 5");
+                    table.ForeignKey(
+                        name: "FK_Coachs_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trainees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(3,2)", precision: 3, scale: 2, nullable: true),
+                    Height = table.Column<decimal>(type: "decimal(3,2)", precision: 3, scale: 2, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainees", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Trainees_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TraineeMemberships",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TraineeID = table.Column<int>(type: "int", nullable: false),
+                    MembershipPlanID = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    MembershipId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TraineeMemberships", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TraineeMemberships_MembershipPlans_MembershipPlanID",
+                        column: x => x.MembershipPlanID,
+                        principalTable: "MembershipPlans",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TraineeMemberships_Memberships_MembershipId",
+                        column: x => x.MembershipId,
+                        principalTable: "Memberships",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_TraineeMemberships_Trainees_TraineeID",
+                        column: x => x.TraineeID,
+                        principalTable: "Trainees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coachs_UserID",
+                table: "Coachs",
+                column: "UserID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MembershipPlans_MembershipID",
+                table: "MembershipPlans",
+                column: "MembershipID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TraineeMemberships_MembershipId",
+                table: "TraineeMemberships",
+                column: "MembershipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TraineeMemberships_MembershipPlanID",
+                table: "TraineeMemberships",
+                column: "MembershipPlanID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TraineeMemberships_TraineeID",
+                table: "TraineeMemberships",
+                column: "TraineeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trainees_UserID",
+                table: "Trainees",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coachs");
+
+            migrationBuilder.DropTable(
+                name: "TraineeMemberships");
+
+            migrationBuilder.DropTable(
+                name: "MembershipPlans");
+
+            migrationBuilder.DropTable(
+                name: "Trainees");
+
+            migrationBuilder.DropTable(
+                name: "Memberships");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
