662baa395c1ff72f4f6a3c620d118cef8e8516da
ahmed khfaga <idkmaster89@gmail.com>
2026-01-03 00:37:13 +0200

    add enum for weekday & level

---
+++ FitZoneGraduationProject/FitZon.Core/Enums/BaseEnum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitZone.Core.Enums
+{
+    public enum UserRole
+    {
+        Trainee = 0,
+        Coach = 1,
+        Admin = 2
+    }
+
+    public enum WeekDay
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+
+    public enum DifficultyLevel
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
