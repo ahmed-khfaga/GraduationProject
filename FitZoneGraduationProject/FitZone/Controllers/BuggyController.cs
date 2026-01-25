6d2938c91329c262472b1226711cc9c8fac8f166
ahmed khfaga <idkmaster89@gmail.com>
2026-01-25 09:41:51 +0200

    adding error type in buggy

---
+++ FitZoneGraduationProject/FitZone/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+﻿using FitZone.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitZone.APIs.Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+        private readonly FitContext _dbcontext;
+
+        public BuggyController(FitContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound() 
+            => NotFound(); // 404
+
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()  
+            => BadRequest(); // 400
+
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+            => Ok(_dbcontext.Users.Find(6000).ToString());
+
+
+
+
+        [HttpGet("bad-request/{id}")] // Make Class To Solve it manually [validation error]
+        public ActionResult GetBadRequestValidation(int id)
+            => Ok(_dbcontext.Users.Find(id));
+
+    }
+}
